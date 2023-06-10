bc51372d045299c89f06e45fe767c9dd4a9298fb
Mrinmoy Goswami <you@example.com>
2023-06-10 13:09:24 +0530

    first com

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect, useMemo } from "react";
+import { Task } from "./Tasks.js";
+import { memo } from "react";
+import  ".//App.css"
 
 function App() {
+  const [input, setInput] = useState("");
+  const [level, setLevel] = useState(1);
+  const [index, setIndex] = useState(0);
+  const [accuracy, setAccuracy] = useState(100);
+  const [time,setTime] = useState(0);
+  const [speed,setSPeed] = useState(0);
+  const [startCounting,setStartCounting] = useState(false);
+  const [words, setWords] = useState(Task[index]);
+  const [done,setDone] = useState(false);
+  const [count,setCount] = useState(0);
+  
+
+
+// useEffect(()=>{
+//  setTyped(input);
+// },[input])
+
+
+// useEffect(()=>{
+//   if(startCounting){
+//     setInterval(()=>{
+//      setTime((time)=>time+1);  
+//     },1000)
+//   }
+// })
+const handleType = (e)=> {
+  e.preventDefault();
+  setStartCounting(true);
+    setInput(e.target.value);
+    
+    setCount((count)=>count+1);
+    console.log(input);
+    
+    
+    const accuracyPercent = Math.round(
+      (input.length/count)*100
+      )
+      if(isNaN(accuracyPercent)){
+        setAccuracy(0);
+      }
+      else{
+        setAccuracy(accuracyPercent)
+      }
+      if(input === words && index !== Task.length-1){
+        setIndex((index)=>index+1);
+        setLevel((lev)=>lev+1)
+        setWords(Task[index+1])
+      setInput("");
+      setAccuracy(accuracy);
+      setCount(0);
+      setStartCounting(false);
+    }
+    if(input === words && index === Task.length-1){
+      setWords("Ho gaya sara so jao !");
+      setInput("");
+      e.preventDefault();
+      setDone(true);
+    }
+
+    }
+    
+   
+    const Results = ()=>{
+      setSPeed(input.length/time);
+    }
+    
+
+
+  // const correctTyped = () => {
+    
+ 
+  // };
+    // const calculateAccuracy = (e)=>{
+      // const correctWords = correctTyped();
+    
+    // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center flex-col w-screen  h-screen">
+      <h1 className="mt-5 mb-10">Touch Typer</h1>
+      <h3>Level {level}/10</h3>
+      <div className="mb-5 border-solid border-2 border-black w-1/2 p-4 rounded-xl">
+        <p id="para"  >{words}</p>
+      </div>
+      <textarea
+        onChange={handleType}
+        // onFocus={Speed}
+        onBLur={Results}
+        className="border-solid border-2 border-black w-1/2 p-5 text-xl"
+        value={input}
+      ></textarea>
+      <p>{  done ? `Go`:`Accuracy : ${accuracy}%`}</p>
+      <p>Speed : {time} WPM</p>
     </div>
   );
 }
---
+++ src/Tasks.js
@@ -0,0 +1,14 @@
+ const Task = [
+   "In the end, it's not the years in your life that count. It's the life in your years.",
+  "The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.",
+  "Life is really simple, but we insist on making it complicated.",
+  "Life is 10% what happens to us and 90% how we react to it.",
+  "The biggest adventure you can take is to live the life of your dreams.",
+  "Life is either a daring adventure or nothing at all.",
+  "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+  "The purpose of our lives is to be happy.",
+  "Life isn't about finding yourself. It's about creating yourself.",
+  "Life is too important to be taken seriously."
+]
+
+export {Task}
